4db85480045c3042e9924edd5d802c62c59b7038
alexfrolty <alexfrolty@gmail.com>
2023-04-06 09:58:42 +0300

    admin ordering

--- testovoe/treemenu/admin.py
+++ testovoe/treemenu/admin.py
@@ -5,7 +5,8 @@ from treemenu.models import Menu
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent', 'url', 'named_url')
-    list_filter = ('parent',)
+    list_filter = ('menu_name', 'parent')
     search_fields = ('name', 'url', 'named_url')
+    ordering = ('menu_name', 'parent')
 
 
